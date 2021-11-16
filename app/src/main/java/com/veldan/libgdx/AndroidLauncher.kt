baa6d510b92891effeb0e0d091ac3eae284bf5cb
Veldan <valdon1202@gmail.com>
2021-11-16 18:19:48 +0200

    Done 2

--- app/src/main/java/com/veldan/libgdx/AndroidLauncher.kt
+++ app/src/main/java/com/veldan/libgdx/AndroidLauncher.kt
@@ -2,6 +2,7 @@ package com.veldan.libgdx
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
